48314edae174e6609e468a5040451b9041a19f95
Alina Kachur <alinakachur32@gmail.com>
2022-12-22 16:09:06 +0300

    feat(ui): add barrel-pattern

---
+++ src/ui/index.ts
@@ -0,0 +1,23 @@
+import { H1, H2, H3, S1, B1, B2 } from "./typography";
+import { lightTheme, darkTheme } from "./theme";
+import { resetCSS } from "./resetCSS";
+import { Media } from "./media";
+import { Indent } from "./indents";
+import { GlobalStyles } from "./globalStyles";
+import { Color } from "./colors";
+
+export {
+  H1,
+  H2,
+  H3,
+  S1,
+  B1,
+  B2,
+  lightTheme,
+  darkTheme,
+  resetCSS,
+  Media,
+  Indent,
+  GlobalStyles,
+  Color,
+};
