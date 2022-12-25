b90bb06d93a188731f20b9595303cd2e05fdbbf5
Alina Kachur <alinakachur32@gmail.com>
2022-12-25 12:54:23 +0300

    feat(components): add LogoIcon, change styles Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,17 @@
 import { Link } from "react-router-dom";
 import { ROUTE } from "../../router";
 import { StyledSearch } from "../Search/styles";
-
-import { StyledHeader } from "./styles";
+import { ReactComponent as LogoIcon } from "../../assets/icons/logo.svg";
+import { StyledHeader, StyledLink } from "./styles";
 
 export const Header = () => {
   return (
     <StyledHeader>
+      <StyledLink to={ROUTE.HOME}>
+        <LogoIcon />
+      </StyledLink>
       <StyledSearch type={"text"} placeholder="Search..." />
       <Link to={ROUTE.FAVORITES}>Favorites</Link>
-      <Link to={ROUTE.HOME}>Home</Link>
     </StyledHeader>
   );
 };
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,11 +1,17 @@
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import { Color } from "../../ui/colors";
 
 const StyledHeader = styled.header`
-  min-height: 96px;
   background-color: ${Color.WHITE_HEADER};
   display: grid;
-  grid-template-columns: 1fr 2fr 1fr;
-  width: clamp(320px, 1120px, 100%);
+  grid-template-columns: 1fr 4fr 1fr;
+  align-items: center;
+  width: 100%;
+  padding: 20px 32px;
 `;
-export { StyledHeader };
+
+const StyledLink = styled(NavLink)`
+  justify-self: flex-start;
+`;
+export { StyledHeader, StyledLink };
