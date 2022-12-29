3a48ea3e747fb1856dbec162c23a90a9bd1b3f69
Alina Kachur <alinakachur32@gmail.com>
2022-12-29 17:57:48 +0300

    feat(src): add environment firebase

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAmD1hw2esmfVe96zXIkOOCh8qfR_SuuVs",
-  authDomain: "fe-87-5c214.firebaseapp.com",
-  projectId: "fe-87-5c214",
-  storageBucket: "fe-87-5c214.appspot.com",
-  messagingSenderId: "587797612570",
-  appId: "1:587797612570:web:79f0a38df7b7206d2669a3",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
