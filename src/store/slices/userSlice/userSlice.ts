c75093b25fea99f8d6dd5e02b4dc2539c96d256c
Alina Kachur <alinakachur32@gmail.com>
2022-12-29 20:31:52 +0300

    feat(store): change redusers in  userSlice

--- src/store/slices/userSlice/userSlice.ts
+++ src/store/slices/userSlice/userSlice.ts
@@ -1,31 +1,29 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface IUser {
-  name: string;
-  surname: string;
-  email: string;
-  isAuth: boolean;
+  email: null | string;
+  id: null | string;
 }
 
 const initialState: IUser = {
-  name: "",
-  surname: "",
-  email: "",
-  isAuth: false,
+  email: null,
+  id: null,
 };
 
 export const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    setUser(state, action) {
-      (state.name = action.payload.name),
-        (state.surname = action.payload.surname),
-        (state.email = action.payload.email),
-        (state.isAuth = !state.isAuth);
+    setUser(state, { payload }) {
+      state.email = payload.email;
+      state.id = payload.id;
+    },
+    removeUser(state) {
+      state.email = null;
+      state.id = null;
     },
   },
 });
 
+export const { setUser, removeUser } = userSlice.actions;
 export default userSlice.reducer;
-export const { setUser } = userSlice.actions;
