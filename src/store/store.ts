000973b563741e97b269c27dabef655a24f98106
Alina Kachur <alinakachur32@gmail.com>
2023-01-07 18:23:47 +0300

    feat(store): add newsSlice

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import articleReducer from "./slices/articlesSlice/articlesSlice";
 import themeReducer from "./slices/themeSlice/themeSlice";
 import userReducer from "./slices/userSlice/userSlice";
+import newsReducer from "./slices/newsSlice/newsSlice";
 
 export const store = configureStore({
   reducer: {
     theme: themeReducer,
     user: userReducer,
     articles: articleReducer,
+    news: newsReducer,
   },
 });
 
