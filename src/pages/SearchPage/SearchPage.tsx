0a4ad575b785cf49e498687a9723d49b6c00833c
Alina Kachur <alinakachur32@gmail.com>
2023-01-13 11:27:43 +0300

    feat(pages): add displaying search results in SearchPage

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,3 +1,23 @@
+import { ArticlesList, Title } from "components";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { spaceNewsAPI } from "services";
+import { useAppDispatch, useAppSelector } from "store";
+import { getNewsArticlesBySearch } from "store/selectors/searchSelector";
+import { fetchArticlesBySearch } from "store/slices/articlesSlice/articlesSlice";
+import { IArticleAPI } from "types/types";
+import { WrapperSearchPage } from "./styles";
+
 export const SearchPage = () => {
-  return <div>SearchPage</div>;
+  const { search } = useAppSelector(getNewsArticlesBySearch);
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    search.title && dispatch(fetchArticlesBySearch(search.title));
+  }, [dispatch, search]);
+  return (
+    <WrapperSearchPage>
+      <Title>Search results "{search.title ? search.title : ""}"</Title>
+      <ArticlesList />
+    </WrapperSearchPage>
+  );
 };
---
+++ src/pages/SearchPage/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const WrapperSearchPage = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+export { WrapperSearchPage };
